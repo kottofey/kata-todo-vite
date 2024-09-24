cd98a9bee44ce9b7fb018f43e419553800e1638a
Kottofey (BigBrother) <romeokontakt@gmail.com>
2024-09-24 14:45:37 +0300

    [fix] Refactoring
    Key is calculated from hash of date created. Description may macth in different items thus hash will be same.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,7 @@ import { Component } from 'react';
 import NewTaskForm from '../NewTaskForm';
 import TaskList from '../TaskList';
 import Footer from '../Footer';
-import reducedHash from '../../helpers/reducedHash.js';
+import reducedHash from '../../helpers/reducedHash';
 
 export default class App extends Component {
   state = {
@@ -12,13 +12,13 @@ export default class App extends Component {
       { taskState: 'done', description: 'DONE!', created: '09-19-2024 01:24:00' },
       // { taskState: 'editing', description: 'Editing', created: '11-12-2024 12:00' },
       { taskState: 'active', description: 'To be done...', created: '9-12-2024 12:00' },
-      { description: 'No defined taskState task', created: '9-12-2024 12:00' },
+      { description: 'No defined taskState task', created: '1-12-2024 12:00' },
     ],
   };
 
   deleteItem = (id) => {
     this.setState(({ todoItems }) => {
-      const idx = todoItems.findIndex((item) => reducedHash(item.description) === id);
+      const idx = todoItems.findIndex((item) => reducedHash(item.created) === id);
 
       return {
         todoItems: [
